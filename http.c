f98e859b0b66c2af72691e604ee126a95972f586
Greg Hazel <ghazel@gmail.com>
2017-05-21 15:50:08 -0700

    EVHTTP_CON_OUTGOING as well

--- http.c
+++ http.c
@@ -2576,6 +2576,7 @@ evhttp_connection_new_(struct event_base *base, struct bufferevent* bev)
 		 * can start sending requests on.
 		 */
 		evcon->state = EVCON_IDLE;
+		evcon->flags |= EVHTTP_CON_OUTGOING;
 	}
 
 	return (evcon);
